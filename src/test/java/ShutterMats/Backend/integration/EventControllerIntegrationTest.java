65bb3e4ccfad83f9827f52ec4e07dd65b1e5fd22
AidaG91 <aida.garmus@gmail.com>
2026-07-25 09:56:58 +0200

    test: add integration test - getEvents

---
+++ src/test/java/ShutterMats/Backend/integration/EventControllerIntegrationTest.java
@@ -0,0 +1,42 @@
+package ShutterMats.Backend.integration;
+
+import ShutterMats.Backend.entity.Event;
+import ShutterMats.Backend.repository.EventRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+public class EventControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @BeforeEach
+    void setup() {
+        eventRepository.deleteAll();
+
+        Event event = new Event(null, "Open BJJ", LocalDate.now(), "Madrid", null, "Desc");
+
+        eventRepository.save(event);
+    }
+
+    @Test
+    void getEvents_returns200AndList() throws Exception {
+        mockMvc.perform(get("/api/events"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("Open BJJ"));
+    }
+}
